760ee4b3d2b8807ef26ab2245628c8db7c6e7a44
sjmy <23250749+sjmy@users.noreply.github.com>
2025-07-12 19:16:16 -0700

    KnightNode object complete

--- KnightNode.js
+++ KnightNode.js
@@ -1,35 +1,42 @@
-// KnightNode object contains a position (coordinate of the square on the board) and eight move attributes
+// KnightNode object contains a position (coordinate of the square on the board) and an array of legal moves
 // Requires a coordinate parameter upon creation in format [x, y]
-// Move attributes are calculated from the position. Impossible moves are set to null
+// Moves are calculated from the position. Impossible moves are set to null
 
 export default function KnightNode(coordinate) {
   const position = coordinate;
-  const possibleMoves = getMoves(); // Array of possible moves instead of an attribute for each one? Then I can iterate through them
-  const one = move(1, 2);
-  const two = move(1, -2);
-  const three = move(2, 1);
-  const four = move(2, -1);
-  const five = move(-1, 2);
-  const six = move(-2, 1);
-  const seven = move(-1, -2);
-  const eight = move(-2, -1);
-
-  function move(x, y) {
-    if (
-      position[0] + x > 7 ||
-      position[0] + x < 0 ||
-      position[1] + y > 7 ||
-      position[1] + y < 0
-    ) {
-      return null;
-    }
-
-    return [position[0] + x, position[1] + y];
-  }
+  const moves = getMoves();
 
   function getMoves() {
-    for (let i = 1; i <= 8; i++) {}
+    const legalMoves = [];
+    const potentialMoves = [
+      [1, 2],
+      [1, -2],
+      [2, 1],
+      [2, -1],
+      [-1, 2],
+      [-2, 1],
+      [-1, -2],
+      [-2, -1],
+    ];
+
+    for (let i = 0; i < potentialMoves.length; i++) {
+      const x = potentialMoves[i][0];
+      const y = potentialMoves[i][1];
+
+      if (
+        position[0] + x > 7 ||
+        position[0] + x < 0 ||
+        position[1] + y > 7 ||
+        position[1] + y < 0
+      ) {
+        legalMoves.push(null);
+      } else {
+        legalMoves.push([position[0] + x, position[1] + y]);
+      }
+    }
+
+    return legalMoves;
   }
 
-  return { position, one, two, three, four, five, six, seven, eight };
+  return { position, moves };
 }
